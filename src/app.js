2598de1e89404afd4aaff66788c7ab8f152df3e2
Kyle Cho <kylecho.work@gmail.com>
2015-12-26 15:23:40 -0800

    Implement routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-var React = require('react')
+import Router from './router'
+import styles from './styles/main.styl'
 
-var Hello = React.createClass({
-  render: function () {
-    return <div>Hello, {this.props.name}</div>
+window.app = {
+  init () {
+    this.router = new Router()
+    this.router.history.start()
   }
-})
+}
 
-React.render(<Hello name="Kyle"/>, document.body)
+window.app.init()
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,5 @@ var getConfig = require('hjs-webpack')
 module.exports = getConfig({
   in: 'src/app.js',
   out: 'public',
-  clearBeforeBuild: true
+  clearBeforeBuild: true,
 })
