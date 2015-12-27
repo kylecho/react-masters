281f3d496952e215ff71871ebcda7cf70156bbb1
Kyle Cho <kylecho.work@gmail.com>
2015-12-26 21:14:32 -0800

    Add octicons for use of icons

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -12,7 +12,7 @@ export default React.createClass({
         <h2>Repos</h2>
         <ul>
           {repos.map((repo) => {
-            return (<li><a href="">{repo.full_name}</a></li>)
+            return (<li key={repo.id}><a href="">{repo.full_name}</a></li>)
           })}
         </ul>
       </div>
