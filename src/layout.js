34b665d0ed683e5624182d0cf12f82340809b021
Kyle Cho <kylecho.work@gmail.com>
2015-12-26 16:28:02 -0800

    Add localLinks to layout for client-side routing

--- src/layout.js
+++ src/layout.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import localLinks from 'local-links'
 
 export default React.createClass({
+  onClick (event) {
+    const pathname = localLinks.getLocalPathname(event)
+
+    if (pathname) {
+      event.preventDefault()
+      app.router.history.navigate(pathname)
+    }
+  },
+
   render () {
     return (
-      <div>
+      <div onClick={this.onClick}>
         <nav className='top-nav top-nav-light cf' role='navigation'>
           <input id='menu-toggle' className='menu-toggle' type='checkbox'/>
           <label htmlFor='menu-toggle'>Menu</label>
