d13b0bf54b27a5eb81cbf203ff9cc551889172c2
Kyle Cho <kylecho.work@gmail.com>
2015-12-26 16:59:17 -0800

    Implement nav-helper for internal routing

--- src/layout.js
+++ src/layout.js
@@ -1,32 +1,24 @@
 import React from 'react'
-import localLinks from 'local-links'
+import NavHelper from './components/nav-helper'
 
 export default React.createClass({
-  onClick (event) {
-    const pathname = localLinks.getLocalPathname(event)
-
-    if (pathname) {
-      event.preventDefault()
-      app.router.history.navigate(pathname)
-    }
-  },
 
   render () {
     return (
-      <div onClick={this.onClick}>
+      <NavHelper>
         <nav className='top-nav top-nav-light cf' role='navigation'>
           <input id='menu-toggle' className='menu-toggle' type='checkbox'/>
           <label htmlFor='menu-toggle'>Menu</label>
           <ul className='list-unstyled list-inline cf'>
             <li>Labelr</li>
             <li><a href='/repos'>Repos</a></li>
-            <li className='pull-right'><a href='/logout'>Logout</a></li>
+            <li className='pull-right'><a href='/'>Logout</a></li>
           </ul>
         </nav>
         <div className='container'>
           {this.props.children}
         </div>
-      </div>
+      </NavHelper>
     )
   }
 })
--- src/pages/public.js
+++ src/pages/public.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import NavHelper from '../components/nav-helper'
 
 export default React.createClass({
   render () {
     return (
-      <div className='container'>
+      <NavHelper className='container'>
         <header role='banner'>
           <h1>Labelr</h1>
         </header>
         <div>
           <p>We label stuff for you, because, we can&trade;</p>
-          <a href='/login' className='button button-large'>
+          <a href='/repos' className='button button-large'>
             <span className='mega-octicon octicon-mark-github'></span> Login with GitHub
           </a>
         </div>
-      </div>
+      </NavHelper>
     )
   }
 })
