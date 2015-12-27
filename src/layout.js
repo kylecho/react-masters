004e4e14e370c12c561f339580295060d955def0
Kyle Cho <kylecho.work@gmail.com>
2015-12-26 19:47:02 -0800

    Implement binding to a model to watch for changes on any props

--- src/layout.js
+++ src/layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 import NavHelper from './components/nav-helper'
 
 export default React.createClass({
+  mixins: [ampersandMixin],
+
+  displayName: 'Layout',
 
   render () {
+    const {me} = this.props
+
     return (
       <NavHelper>
         <nav className='top-nav top-nav-light cf' role='navigation'>
@@ -12,7 +18,7 @@ export default React.createClass({
           <ul className='list-unstyled list-inline cf'>
             <li>Labelr</li>
             <li><a href='/repos'>Repos</a></li>
-            <li className='pull-right'><a href='/logout'>Logout</a></li>
+            <li className='pull-right'><a href='/logout'>Logout</a> {me.login}</li>
           </ul>
         </nav>
         <div className='container'>
