e3ef60859ed7e372f16088ca78379c9da5acd1ca
Kyle Cho <kylecho.work@gmail.com>
2015-12-28 11:46:41 -0800

    Implement pre-rendering contents

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
+require('babel/register')
 var getConfig = require('hjs-webpack')
+var React = require('react')
+var PublicPage = require('./src/pages/public')
+var Layout = require('./src/layout')
 
 module.exports = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   html: function (context) {
+    const publicPage = React.renderToString(React.createElement(PublicPage))
+    const layoutPage = React.renderToString(React.createElement(Layout, {me: {}}))
+
     return {
-      'index.html': context.defaultTemplate(),
-      '200.html': context.defaultTemplate()
+      'index.html': context.defaultTemplate({html: publicPage}),
+      '200.html': context.defaultTemplate({html: layoutPage})
     }
   }
 })
